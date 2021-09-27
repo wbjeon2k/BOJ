a873a3d6dab117b848c24e408472282d88e83380
wbjeon2k <wbjeon2k@users.noreply.github.com>
2021-09-28 01:59:07 +0900

    Update SCC-Kosaraju.cpp

--- Library/SCC-Kosaraju.cpp
+++ Library/SCC-Kosaraju.cpp
@@ -1,3 +1,4 @@
+//https://www.acmicpc.net/problem/1648
 
 #define _CRT_SECURE_NO_WARNINGS
 #include <bits/stdc++.h>
@@ -17,15 +18,15 @@ const ll INF = INT_MAX / 4;
 int visited[MSIZE + 1];
 int x, y, r, v, e;
 vector<vector<int>> scc;
-vector<vector<int>> vt;
-vector<vector<int>> rvt;
+vector<vector<int>> adjlist;
+vector<vector<int>> rev_adjlist;
 stack<int> st;
 bool cmp(vector<int> x, vector<int> y) {
     return x[0] < y[0];
 }
 void dfs(int v) {
     visited[v] = true;
-    for (int next : vt[v]) {
+    for (int next : adjlist[v]) {
         if (visited[next])
             continue;
         dfs(next);
@@ -35,7 +36,7 @@ void dfs(int v) {
 void func(int v, int c) {
     visited[v] = true;
     scc[c].push_back(v);
-    for (int next : rvt[v]) {
+    for (int next : rev_adjlist[v]) {
         if (visited[next])
             continue;
         func(next, c);
@@ -68,12 +69,12 @@ int main() {
     //ifstream cin; cin.open("input.txt");
 
     scanf("%d%d", &v, &e);
-    vt.resize(v + 1);
-    rvt.resize(v + 1);
+    adjlist.resize(v + 1);
+    rev_adjlist.resize(v + 1);
     for (int i = 0; i < e; i++) {
         scanf("%d%d", &x, &y);
-        vt[x].push_back(y);
-        rvt[y].push_back(x);
+        adjlist[x].push_back(y);
+        rev_adjlist[y].push_back(x);
     }
 
     kosaraju();
